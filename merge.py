3dba55283ce954af8116bba8ff064f6b7b9cbc6a
kxy <1838885106@qq.com>
2024-10-22 12:47:26 +0800

    merge

--- merge.py
+++ merge.py
@@ -1,9 +1,13 @@
 import os
 
-if os.path.exists("./merge"):
-    os.remove("./merge")
-merge = open("./merge", 'a+', encoding='utf-8')
+if os.path.exists("./merge.json"):
+    os.remove("./merge.json")
+merge = open("./merge.json", 'a+', encoding='utf-8')
+merge.write('[')
 for root, dirs, files in os.walk("./sources"):
     for file in files:
         f = open(os.path.join(root, file), "r", encoding='utf-8')
         merge.write(f.read())
+        if file != files[-1]:
+            merge.write(',')
+merge.write(']')
